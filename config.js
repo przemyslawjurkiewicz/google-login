c02266cb4a64f1c580483a60c06d4cf291c4c61d
Przemysław Jurkiewicz <przemko.jurkiewicz@gmail.com>
2019-04-01 15:03:33 +0200

    Add google auth

---
+++ config.js
@@ -0,0 +1,5 @@
+module.exports = {
+    'GOOGLE_CLIENT_ID': '747673976976-3dr9jbimatvvdjhadgijfed6idm5ag0t.apps.googleusercontent.com',
+    'GOOGLE_CLIENT_SECRET': 'ZFMIOlwelXtb0-ceuO2On130',
+    'CALLBACK_URL': 'http://localhost:3000/auth/google/callback'
+};
--- server.js
+++ server.js
@@ -1,8 +1,14 @@
 var express = require('express');
+var passport = require('passport');
+var GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
+var config = require('./config');
 var app = express();
+var googleProfile = {};
 
 app.set('view engine', 'pug');
 app.set('views', './views');
+app.use(passport.initialize());
+app.use(passport.session());
 
 app.use(express.static(__dirname + '/views'));
 
@@ -17,9 +23,42 @@ app.get('/userform', function (req, res) {
   })
 })
 
-app.get('/auth/google', function (req, res) {
-  res.render('googlelogin.pug')
-})
+passport.serializeUser(function (user, done) {
+  done(null, user);
+});
+passport.deserializeUser(function (obj, done) {
+  done(null, obj);
+});
+
+passport.use(new GoogleStrategy({
+    clientID: config.GOOGLE_CLIENT_ID,
+    clientSecret: config.GOOGLE_CLIENT_SECRET,
+    callbackURL: config.CALLBACK_URL
+  },
+  function (accessToken, refreshToken, profile, cb) {
+    googleProfile = {
+      id: profile.id,
+      displayName: profile.displayName
+    };
+    cb(null, profile);
+  }
+));
+
+app.get('/logged', function (req, res) {
+  res.render('logged.pug', {
+    user: googleProfile
+  });
+});
+
+app.get('/auth/google',
+  passport.authenticate('google', {
+    scope: ['profile', 'email']
+  }));
+app.get('/auth/google/callback',
+  passport.authenticate('google', {
+    successRedirect: '/logged',
+    failureRedirect: '/'
+  }));
 
 var server = app.listen(3000, 'localhost', function () {
   var host = server.address().address;
